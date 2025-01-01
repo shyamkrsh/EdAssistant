c8d9260d65ce500f21bc8d62cee1482e0e7f0a06
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-01 17:47:22 +0530

    navbar fixed

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaUser } from "react-icons/fa";
 
 function Navbar() {
     return (
-        <header className='shadow-lg flex items-center justify-between sticky top-0 py-2 px-4' style={{ background: 'linear-gradient(90deg, #001116, #13035c, #030352)' }}>
+        <header className='shadow-lg flex items-center justify-between fixed top-0 py-2 px-4 w-[100%]' style={{ background: 'linear-gradient(90deg, #001116, #13035c, #030352)' }}>
             <div>
                 <h1 className='text-3xl font-bold text-slate-500'>EdAssistant</h1>
             </div>
