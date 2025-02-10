dfd78a1da4d9161e563249de3376b5a59f1d8526
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-10 20:09:19 +0530

    functionality added

--- frontend/src/Components/QAPlace.jsx
+++ frontend/src/Components/QAPlace.jsx
@@ -31,14 +31,17 @@ function QAPlace() {
           "parts": [
             {
               "text": `Questions: ${value}, Follow these rules: 
+              Note: You are a chatbot developed to assist user about SBTE Bihar.
                 1️⃣ I am a chatbot for SBTE Bihar.
                 2️⃣ Answer as concisely as possible.
-                3️⃣ If you don’t know, refer to SBTE’s official website.
+                3️⃣ If you don’t know, refer to SBTE’s official website and provide link.
                 4️⃣ If asked about 'New Government Polytechnic', refer to its official website.
                 5️⃣ If asked about the development team, reply: "I am a chatbot developed for SBTE assistance."
                 6️⃣ Respond in the same language as the question.
-                7️⃣ Do not repeat instructions or the question in your response.`
-
+                7️⃣ Do not repeat instructions or the question in your response.
+                8. If user asks other questions outside of information associated with sbte the please don't give response.
+                9. If user asks about any questions related to any study subjects then please don't give response.
+                10. if user asks outside the given instruction please can't give response`
             }
           ]
         }]
@@ -57,43 +60,44 @@ function QAPlace() {
 
   return (
     <>
-      <div className='w-[100%] p-2 bg-cyan-50 overflow-x-hidden overflow-y-auto' style={{ maxHeight: 'calc(100vh - 6.8rem)', minHeight: 'calc(100vh - 6.8rem)' }} ref={chatRef} >
+      <div className='w-[100%] p-2 bg-cyan-50 overflow-x-hidden overflow-y-auto text-black' style={{ maxHeight: 'calc(100vh - 6.8rem)', minHeight: 'calc(100vh - 6.8rem)', scrollbarWidth: 'none', msOverflowStyle: 'none' }} ref={chatRef} >
         {
           questions?.map((question, index) => {
             return (
               <div key={index}>
-                <div className='w-[100%] p-1 flex justify-end'>
-                  <div className='max-w-[60%]  px-1'>
-                    <p className='text-slate-600 bg-cyan-200 p-1 rounded-lg'>{question}</p>
+                <div className='w-[100%] flex justify-end'>
+                  <div className="chat chat-end">
+                    <div className="chat-bubble bg-cyan-200 text-slate-700 ">{question}</div>
                   </div>
                 </div>
                 <div className='w-[100%] p-1'>
-                  <h3 className='px-2 font-bold'><img src="https://i.ibb.co/F4hW40tv/robot-3559850.png" className='w-[1.8rem] h-[1.5rem]' /> </h3>
-                  <p className={answers[index] ? 'px-2 mt-1 py-1 bg-white shadow-sm rounded-lg' : 'hidden'} >
-                    {answers[index]}
-                  </p>
-                  {/* <div className="chat chat-end">
-                    <div className="chat-bubble">You underestimate my power!</div>
-                  </div> */}
+                  <h3 className='px-2 font-bold mb-1'><img src="https://i.ibb.co/F4hW40tv/robot-3559850.png" className='w-[1.8rem] h-[1.5rem]' /> </h3>
+                  <div className={answers[index] ? "chat chat-start" : 'hidden'}>
+                    <div className="chat-bubble bg-white shadow-sm text-slate-700">
+                      {answers[index]}
+                    </div>
+                  </div>
                 </div>
               </div>
             )
           })
         }
-        {/* <div className={loading ? 'ms-3 w-[3rem] h-[2rem] bg-slate-100 border grid place-items-center rounded-lg ' : 'hidden'}>
+        <div className={loading ? 'ms-3 w-[3rem] h-[2rem] bg-slate-100 border grid place-items-center rounded-lg ' : 'hidden'}>
           <span className="loading loading-dots loading-md text-slate-950"></span>
-        </div> */}
+        </div>
 
       </div>
 
-      <div className='w-[100%] h-[3rem] fixed bottom-2 bg-cyan-50 flex items-center justify-center'>
-        <div className='flex items-center bg-white justify-between w-[85%]' style={{ borderRadius: '2rem', border: '1px solid gray' }}>
-          <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder='Ask your questions ...' className='h-[2.3rem] px-4 bg-white  outline-none w-full text-slate-800' style={{ borderRadius: '2rem' }} />
-          <div className='searchBtn  p-1  rounded-full m-0.5 cursor-pointer hover:opacity-80' onClick={handleSubmit}>
-            <LuSendHorizontal className='text-white text-3xl' />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className='w-[100%] h-[3rem] fixed bottom-2 bg-cyan-50 flex items-center justify-center'>
+          <div className='flex items-center bg-white justify-between w-[85%]' style={{ borderRadius: '2rem', border: '1px solid gray' }}>
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} placeholder='Ask your questions ...' className='h-[2.3rem] px-4 bg-white  outline-none w-full text-slate-800' style={{ borderRadius: '2rem' }} />
+            <button type='submit' className='searchBtn  p-1  rounded-full m-0.5 cursor-pointer hover:opacity-80' onClick={handleSubmit}>
+              <LuSendHorizontal className='text-white text-3xl' />
+            </button>
           </div>
         </div>
-      </div>
+      </form>
     </>
   )
 }
